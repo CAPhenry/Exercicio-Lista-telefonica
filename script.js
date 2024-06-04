efd523acbba65e67e9b2c8bdcfe3acbaf34d6fbb
henry <henrybotelho2017@outlook.com>
2024-06-04 05:50:08 -0300

    Fix no js

--- script.js
+++ script.js
@@ -4,34 +4,37 @@
 
 
 
-const form = document.getElementById("form")
-const InputNome = document.getElementById("input-nome")
-const InputTel = document.getElementById("input-tel")
+const form = document.getElementById("form");
+const InputNome = document.getElementById("input-nome");
+const InputTel = document.getElementById("input-tel");
 let linhas = '';
 
 let numeros = [];
 
 form.addEventListener("submit",function(e){
     e.preventDefault();
-    AdicionarLinhas()
-    AtualizarTabela()
+    AdicionarLinhas();
+    AtualizarTabela();
 })
 
 AdicionarLinhas = function(){
     if (numeros.includes(InputTel.value)){
-        alert("Ops! Esse número já foi adicionado a lista.")
+        alert("Ops! Esse número já foi adicionado a lista.");
     }else{
-        let line = '<tr>'
-        line += `<td> ${InputNome.value}</td>`
-        line += `<td> ${InputTel.value}</td>`
-        line += `</tr>`
-        linhas += line
-        numeros.push(InputTel.value)
+        let line = '<tr>';
+        line += `<td> ${InputNome.value}</td>`;
+        line += `<td> ${InputTel.value}</td>`;
+        line += `</tr>`;
+        linhas += line;
+        numeros.push(InputTel.value);
     }
+
+    InputNome.value = '';
+    InputTel.value = '';
     
 }
 
 AtualizarTabela = function(){
-    const tabela = document.querySelector("tbody")
-    tabela.innerHTML = linhas
+    const tabela = document.querySelector("tbody");
+    tabela.innerHTML = linhas;
 }
